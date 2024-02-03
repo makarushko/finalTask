26b26dec2704483f18026574aca2286be48fde57
Anna <makarushko7702@gmail.com>
2024-02-04 00:14:02 +0300

    completed task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] initStr;
+Console.WriteLine("Введите массив строк через запятую");
+string str = Console.ReadLine();
+initStr = str.Split(',');
+var result = new string[initStr.Length];
+var realSize = 0;
+foreach (var value in initStr )
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
